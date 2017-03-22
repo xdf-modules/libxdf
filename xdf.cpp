1b271764ae18b81e4e0d2e5d173921b4ae1248eb
Yida Lin <yida.lin@outlook.com>
2017-03-22 01:36:00 -0400

    Fix a bug

--- xdf.cpp
+++ xdf.cpp
@@ -763,7 +763,7 @@ void Xdf::freeUpTimeStamps()
     for (auto &stream : streams)
     {
         //we don't need to keep all the time stamps unless it's a stream with irregular samples
-        if (stream.info.nominal_srate != 0)
+        if (stream.info.nominal_srate != 0 && !stream.time_stamps.empty())
         {
             std::vector<float> nothing;
             //however we still need to keep the first time stamp of each stream to decide at which position the signal should start
